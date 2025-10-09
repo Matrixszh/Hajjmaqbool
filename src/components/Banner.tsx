e6b381c5677ae70faca920186fe5404f0b3e830c
Zain <zainhussaini9898@gmail.com>
2025-10-10 03:19:41 +0530

    added logos and banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,14 +5,16 @@ export function StickyBannerDemo() {
   return (
     <div className="relative flex h-[10vh] w-full flex-col overflow-y-auto">
       <StickyBanner className="bg-gradient-to-b from-emerald-900 to-black">
-        <p className=" text-xl mx-0 max-w-[90%] text-white drop-shadow-md">
+        <p className="text-xl mx-0 max-w-[90%] text-white drop-shadow-md">
           Hajj Heartline Number{" "}
-          <a href="#" className="text-green-400 transition duration-200 hover:underline">
-            XXXXXXXXXXXXXX
+          <a 
+            href="tel:+966XXXXXXXXXXXX" 
+            className="text-green-400 transition duration-200 hover:underline"
+          >
+            +966 XXXXXXXXX
           </a>
         </p>
       </StickyBanner>
     </div>
   );
 }
-
